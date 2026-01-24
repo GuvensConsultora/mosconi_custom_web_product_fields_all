e4822b2485e8e09e7e26e490025ffbbf75fa4457
guvensconsultora <guvensconsultora@gmail.com>
2026-01-24 19:58:11 -0300

    Desplegar modulo

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Mosconi - Mostrar Color y SKU de Variante",
-    "version": "18.0.1.0.0",
+    "version": "18.0",
     "category": "Website",
     "summary": "Muestra el nombre del color y la referencia al seleccionar una variante en la tienda",
     "depends": ["website_sale"],
